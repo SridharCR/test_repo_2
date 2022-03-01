3fcd05832ed542b278f0cad4c2e76428e68ef27f
Sridhar <crsridhar23@gmail.com>
2022-03-01 07:26:16 +0530

    update details page

--- ui/src/insurance/update_details.js
+++ ui/src/insurance/update_details.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 
 import Button from '@atlaskit/button/standard-button';
+import Textfield from '@atlaskit/textfield';
 
 import Modal, {
     ModalBody,
@@ -29,15 +30,32 @@ export default function InsuranceUpdate(props) {
                                 Update the customer and insurance data here
                             </ModalTitle>
                         </ModalHeader>
-                        <ModalBody>
-                            <p>{props.data.Customer_id}</p>
+                        <ModalBody>                           
+
+                                <div className='insurance-label'><label>Customer ID</label></div><div className='insurance-inputs'><label>{props.data['Customer_id'] }</label></div>
+                                <div className='insurance-label'><label>Gender</label></div><div className='insurance-inputs'><label>{props.data['Customer_Gender']}</label></div>
+                                <div className='insurance-label'><label>Income group</label></div><div className='insurance-inputs'><Textfield value={props.data['Customer_Income group']}/></div>
+                                <div className='insurance-label'><label>Region</label></div><div className='insurance-inputs'><Textfield value={props.data['Customer_Region']}/></div>
+                                <div className='insurance-label'><label>Marital status</label></div><div className='insurance-inputs'><Textfield value={props.data['Customer_Marital_status']}/></div>
+                                <div className='insurance-label'><label>Policy ID</label></div><div className='insurance-inputs'><label>{props.data['Policy_id']}</label></div>
+                                <div className='insurance-label'><label>Date of purchase</label></div><div className='insurance-inputs'><Textfield value={props.data['Date of Purchase']}/></div>
+                                <div className='insurance-label'><label>Fuel</label></div><div className='insurance-inputs'><Textfield value={props.data['Fuel']}/></div>
+                                <div className='insurance-label'><label>Vehicle Segment</label></div><div className='insurance-inputs'><Textfield value={props.data['VEHICLE_SEGMENT']}/></div>
+                                <div className='insurance-label'><label>Premium</label></div><div className='insurance-inputs'><Textfield value={props.data['Premium']}/></div>
+                                <div className='insurance-label'><label>Bodily injury liability</label></div><div className='insurance-inputs'><Textfield value={props.data['bodily injury liability']}/></div>
+                                <div className='insurance-label'><label>Personal injury protection</label></div><div className='insurance-inputs'><Textfield value={props.data['personal injury protection']}/></div>
+                                <div className='insurance-label'><label>Property damage liability</label></div><div className='insurance-inputs'><Textfield value={props.data['property damage liability']}/></div>
+                                <div className='insurance-label'><label>Collision</label></div><div className='insurance-inputs'><Textfield value={props.data['collision']}/></div>
+                                <div className='insurance-label'><label>Comprehensive</label></div><div className='insurance-inputs'><Textfield value={props.data['comprehensive']}/></div>
+                            
+                            
 
                         </ModalBody>
 
                         <ModalFooter>
-                            <Button appearance="subtle">Cancel</Button>
+                            {/* <Button appearance="subtle">Cancel</Button> */}
                             <Button appearance="warning" onClick={closeModal} autoFocus>
-                                Delete
+                            Update
                             </Button>
                         </ModalFooter>
                     </Modal>
